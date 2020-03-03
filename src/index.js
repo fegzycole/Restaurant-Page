8585d8404d5c21434aafddc335120de5c5cf0032
fegzycole <fergusoniyara@gmail.com>
2020-03-03 13:07:50 +0100

    Add welcome page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import navbar from './modules/navbar';
+import homePage from './modules/home';
 
 const app = document.querySelector('#content');
 
 app.appendChild(navbar());
+app.appendChild(homePage());
